f177b144cfd4172fbfa25a80d1ffe329712621d8
FoxxMD <FoxxMD@users.noreply.github.com>
2023-07-12 09:46:16 -0400

    Implement admin-level submit bypass command

--- src/command/commands/moderation/submitfor.ts
+++ src/command/commands/moderation/submitfor.ts
@@ -6,7 +6,7 @@ import {
     TextInputBuilder,
     TextInputStyle,
     ActionRowBuilder,
-    time
+    time, PermissionFlagsBits, GuildMember
 } from "discord.js";
 import {oneLine} from 'common-tags';
 import {getOrInsertUser, getVideoByVideoId} from "../../../bot/functions/repository.js";
@@ -14,53 +14,35 @@ import {Logger} from "@foxxmd/winston";
 import {Bot} from "../../../bot/Bot.js";
 import {PlatformManager} from "../../../common/contentPlatforms/PlatformManager.js";
 import {markdownTag, timestampToDuration} from "../../../utils/StringUtils.js";
-import dayjs from "dayjs";
 import {addFirehoseVideo} from "../../../bot/functions/firehose.js";
 import {
     AllowedVideoProviders,
-    MinimalCreatorDetails,
     MinimalVideoDetails
 } from "../../../common/infrastructure/Atomic.js";
 import {
-    checkAge,
-    checkLengthConstraints, checkRules,
-    checkSelfPromotion,
-    rateLimitUser
+    checkLengthConstraints
 } from "../../../bot/functions/userSubmissionFuncs.js";
-import {GuildSettings} from "../../../common/db/models/GuildSettings.js";
-import {memberHasRoleType} from "../../../bot/functions/userUtil.js";
-import {ROLE_TYPES} from "../../../common/db/models/SpecialRole.js";
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('submit')
-        .setDescription('Submit a video to MTV')
+        .setName('submit-for')
+        .setDescription('Submit a video to MTV on behalf of a User')
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+        .addUserOption(opt =>
+            opt.setName('user')
+                .setDescription('Discord user to submit on behalf of')
+                .setRequired(true))
         .addStringOption(option => option.setName('url')
             .setDescription('URL of the Video')
             .setRequired(true)),
     async execute(interaction: ChatInputCommandInteraction<CacheType>, logger: Logger, bot: Bot) {
 
-        const user = await getOrInsertUser(interaction.member, interaction.guild);
-        const hasAllowedRole = await memberHasRoleType(ROLE_TYPES.APPROVED, interaction);
-
-        await checkRules(interaction, user);
-        if (interaction.replied) {
-            return;
-        }
-        await checkAge(interaction, user);
-        if (interaction.replied) {
-            return;
+        const behalfOf = interaction.options.getMember('user') as GuildMember;
+        if(interaction.member.user.id === behalfOf.user.id) {
+            return await interaction.reply({content: 'Cannot submit a video on your own behalf', ephemeral: true});
         }
 
-        const guild = await user.getGuild();
-        const limited = await guild.getSettingValue<boolean>(GuildSettings.RATE_LIMIT_MODE);
-
-        if (limited) {
-            await rateLimitUser(interaction, user);
-            if (interaction.replied) {
-                return;
-            }
-        }
+        const user = await getOrInsertUser(behalfOf, interaction.guild);
 
         const url = interaction.options.getString('url');
 
@@ -92,20 +74,6 @@ module.exports = {
                     At least one month must pass between submissions of the same video.`,
                     ephemeral: true
                 });
-            }
-        }
-
-        // can ignore self-promo if user is allowed
-        if (!hasAllowedRole && deets.creator.id !== undefined) {
-            // now check creator popularity (gated by allow role check to reduce platform api calls)
-            const popular = manager.checkPopularity(deets.platform, deets.creator as MinimalCreatorDetails);
-            if (!popular) {
-                // either cannot get popularity from platform (api unsupported) or creator is not popular
-                // so check for self-promo
-                await checkSelfPromotion(interaction, deets.platform, deets.creator as MinimalCreatorDetails, user);
-                if (interaction.replied) {
-                    return;
-                }
             }
         }
 
