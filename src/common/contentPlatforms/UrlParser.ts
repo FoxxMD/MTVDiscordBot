51d8b5cde9db24825ddf27173ecdad8412188fbe
FoxxMD <FoxxMD@users.noreply.github.com>
2023-07-18 17:09:45 -0400

    bug(youtube): Match youtube shorts
    Closes #9

--- src/common/contentPlatforms/UrlParser.ts
+++ src/common/contentPlatforms/UrlParser.ts
@@ -11,7 +11,22 @@
 // but for now it seems best to use the default parser and restrict based on provider keyword with custom domains added?
 
 import videoUrlParser from "js-video-url-parser";
-//import {VideoInfo} from "js-video-url-parser/lib/urlParser.js";
+
+// @ts-ignore
+videoUrlParser.plugins.youtube.parseVideoUrl = function(url) {
+    let match = url.match(
+        /(?:(?:v|vi|be|videos|embed)\/(?!videoseries)|(?:v|ci)=)([\w-]{11})/i
+    );
+    if(match) {
+        return match[1];
+    }
+    // match youtube shorts
+    match = url.match(/youtube.com\/shorts\/([\w-]{11})/i);
+    if(match) {
+        return match[1];
+    }
+    return undefined;
+};
 
 // export const urlParser = (url: string): VideoInfo => {
 //     return videoUrlParser.parse(url);
